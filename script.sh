459fb8a3ac03ea97ad52046615928245bc5c6b0d
root <root@localhost.localdomain>
2018-09-03 13:57:37 -0300

    first commit

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-yum install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
-yum install yum-utils
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm -y
+yum install http://rpms.remirepo.net/enterprise/remi-release-6.rpm -y
+yum install yum-utils -y
 yum-config-manager --enable remi-php70
-yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
